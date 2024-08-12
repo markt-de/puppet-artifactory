c86b2dadddc011cbb6dc701aea6fafbe14a86e76
Frank Wall <fw@moov.de>
2024-08-12 22:38:58 +0200

    APT repo config should respect $manage_repo, refs https://github.com/fervidus/artifactory/pull/24

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -7,25 +7,27 @@ class artifactory::repo::apt () {
 
   include apt
 
-  case $artifactory::edition {
-    'enterprise', 'pro' : {
-      $_url = $artifactory::apt_baseurl_pro
+  if $artifactory::manage_repo {
+    case $artifactory::edition {
+      'enterprise', 'pro' : {
+        $_url = $artifactory::apt_baseurl_pro
+      }
+      default : {
+        $_url = $artifactory::apt_baseurl
+      }
     }
-    default : {
-      $_url = $artifactory::apt_baseurl
-    }
-  }
 
-  apt::source { 'artifactory':
-    location => $_url,
-    release  => $facts['os']['distro']['codename'],
-    repos    => $artifactory::apt_repos,
-    include  => {
-      'src' => false,
-    },
-    key      => {
-      'id'     => $artifactory::apt_key_id,
-      'source' => $artifactory::apt_key_source,
-    },
+    apt::source { 'artifactory':
+      location => $_url,
+      release  => $facts['os']['distro']['codename'],
+      repos    => $artifactory::apt_repos,
+      include  => {
+        'src' => false,
+      },
+      key      => {
+        'id'     => $artifactory::apt_key_id,
+        'source' => $artifactory::apt_key_source,
+      },
+    }
   }
 }
